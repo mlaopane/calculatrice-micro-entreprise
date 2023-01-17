528e3a5dc229a6666afcea491fa6b24cd8e05fa5
mlaopane <mlaopane@gmail.com>
2023-01-17 21:16:52 +0100

    fix: number input behavior

---
+++ src/lib/Input.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  export let name;
+  export let id;
+  export let placeholder = '';
+  export let onInput: (event: {
+    currentTarget: EventTarget & HTMLInputElement;
+  }) => void = undefined;
+  export let value;
+</script>
+
+<input
+  {...$$restProps}
+  {name}
+  {id}
+  {placeholder}
+  bind:value
+  type="text"
+  on:input={onInput}
+/>
+
+<style>
+  input {
+    border-style: solid;
+    border-color: hsla(205, 50%, 30%, 0.9);
+    border-width: 0.2rem;
+    border-radius: 0.2rem;
+    box-sizing: border-box;
+    height: 5.6rem;
+    font-size: 1.8rem;
+    padding: 0px 1.6rem;
+    transition: border-color 150ms;
+  }
+
+  input:focus {
+    outline: 0.2rem solid hsla(205, 90%, 70%, 0.9);
+  }
+
+  input[aria-invalid='true'] {
+    border-color: hsla(350, 50%, 50%, 0.9);
+  }
+
+  input[aria-invalid='true']:focus {
+    outline: 0.2rem solid hsla(350, 90%, 70%, 0.9);
+  }
+</style>
--- src/lib/NumberInput.svelte
+++ src/lib/NumberInput.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
+  import Input from './Input.svelte';
+
   export let name;
-  export let value;
   export let id;
   export let placeholder = '';
+  export let value = 0;
+
+  function isLastCharacterNumber(value: string): boolean {
+    return !isNaN(+value.slice(-1));
+  }
+
+  function extractPreviousValue(value: string): string {
+    return value.slice(0, value.length - 1);
+  }
+
+  function keepNumbersOnly(eventValue: string): number {
+    if (isLastCharacterNumber(eventValue)) {
+      return +eventValue;
+    }
+    return +extractPreviousValue(eventValue);
+  }
+
+  function handleInputEvent(event: { currentTarget: HTMLInputElement }) {
+    console.log(typeof event.currentTarget.value);
+    value = keepNumbersOnly(event.currentTarget.value);
+  }
 </script>
 
-<input type="number" {id} {name} {placeholder} bind:value />
+<Input
+  {...$$restProps}
+  {name}
+  {id}
+  {placeholder}
+  inputmode="numeric"
+  onInput={handleInputEvent}
+  bind:value
+/>
+
+<style>
+</style>
