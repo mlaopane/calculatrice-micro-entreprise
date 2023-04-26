c774217c1c4f65f77489f0dad66b8730d386e10c
mlaopane <mlaopane@gmail.com>
2023-04-27 00:52:01 +0200

    use EventHandler utility in NumberInput

--- src/lib/NumberInput.svelte
+++ src/lib/NumberInput.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  import type { EventHandler } from './utils';
+  type NumberInputEvent = { target: { value: number } };
+
   export let name: string;
   export let id: string;
   export let value = 0;
-  export let onInput: (event: { target: { value: number } }) => void = ({
-    target,
-  }) => {
+  export let onInput: EventHandler<NumberInputEvent> = ({ target }) => {
     value = target.value;
   };
 </script>
---
+++ src/lib/utils.d.ts
@@ -0,0 +1,1 @@
+export type EventHandler<Event> = (event: Event) => void;
