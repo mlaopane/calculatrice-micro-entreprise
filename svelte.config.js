8703cc911e1c4ce25748671128e21c795d0a2c35
Mickaël <mlaopane@gmail.com>
2024-03-05 15:03:59 +0100

    Deploy with github workflows

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,10 @@ import sveltePreprocess from 'svelte-preprocess'
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: sveltePreprocess(),
+  kit: {
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+    }
+  }
 }
